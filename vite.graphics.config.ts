26b2538a426a037c70ad83d760f96527107016bd
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-03-02 02:13:22 +0900

    updated a vite config file

--- vite.graphics.config.ts
+++ vite.graphics.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: resolve(__dirname, "./src/graphics"),
+  base: "./",
   build: {
-    outDir: ".",
-    emptyOutDir: false,
+    outDir: resolve(__dirname, "graphics"),
     rollupOptions: {
       input: {
-        "graphics/graphics": resolve(__dirname, "src/graphics/graphics.html"),
+        graphics: resolve(__dirname, "./src/graphics/graphics.html"),
       },
     },
   },
