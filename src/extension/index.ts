72f78a526a8089af3600d761f5d5966719641f11
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-03-03 06:33:11 +0900

    zoom webhookの下地実装

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,6 +1,10 @@
 import NodeCG from "nodecg/types";
-import { createHmac } from "crypto";
-import { WEBHOOK_SECRET_TOKEN, WEBHOOK_VERIFICATION_TOKEN } from "../../env";
+import { WEBHOOK_VERIFICATION_TOKEN } from "../../env";
+import {
+  ParticipantJoinedInfo,
+  ParticipantLeftInfo,
+  ChatMessage,
+} from "../zoomTypes";
 
 module.exports = (nodecg: NodeCG.ServerAPI) => {
   const router = nodecg.Router();
@@ -13,22 +17,53 @@ module.exports = (nodecg: NodeCG.ServerAPI) => {
       return;
     }
 
-    const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(req.body)}`;
-    const hashForVerify = createHmac("sha256", WEBHOOK_SECRET_TOKEN)
-      .update(message)
-      .digest("hex");
-    const signature = `v0=${hashForVerify}`;
-    if (req.headers["x-zm-signature"] !== signature) {
-      console.error("Failed Verification");
-      res.status(401).send("Unauthorized");
-      return;
+    console.info(`\teventType: ${req.body.event}`);
+    switch (req.body.event) {
+      case "meeting.chat_message_sent": {
+        const info: ChatMessage = req.body.payload.object.chat_message;
+        const sender = info.sender_name;
+        const content = info.message_content;
+        console.log(`\t${sender}: ${content}`);
+        res.json({
+          message: "OK",
+          status: 200,
+        });
+        break;
+      }
+      case "meeting.participant_joined": {
+        const info: ParticipantJoinedInfo = req.body.payload.object.participant;
+        const user_name = info.user_name;
+        const user_id = info.user_id;
+        const join_time = info.join_time;
+        console.info(`\t${join_time}: ${user_name} (${user_id})`);
+        res.json({
+          message: "OK",
+          status: 200,
+        });
+        break;
+      }
+      case "meeting.participant_left": {
+        const info: ParticipantLeftInfo = req.body.payload.object.participant;
+        const user_name = info.user_name;
+        const user_id = info.user_id;
+        const leave_time = info.leave_time;
+        console.info(`\t${leave_time}: ${user_name} (${user_id})`);
+        res.json({
+          message: "OK",
+          status: 200,
+        });
+        break;
+      }
+      case "endpoint.url_validation": {
+        // const payload = req.body.payload;
+        res.json({});
+        break;
+      }
+      default: {
+        console.error("\tUnexpected Event happend");
+      }
     }
-
     res.status(200);
-    res.send({
-      message: "OK",
-      status: 200,
-    });
   });
   nodecg.mount(router);
 };
